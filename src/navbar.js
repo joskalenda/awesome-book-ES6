03e3f94ec9898c9d030b4685ec52a122c52681f2
Topaz Jos <topazjos@Topaz.local>
2022-01-10 12:01:39 +0200

    Creat script for navigation

---
+++ src/navbar.js
@@ -0,0 +1,33 @@
+const ShowList = document.querySelector('#list-link');
+const AddNew = document.querySelector('#addlink');
+const ContactInfo = document.querySelector('#contact-link');
+const BookSection = document.querySelector('.container');
+const MainTitle = document.querySelector('#list-title');
+const AddSection = document.querySelector('.add-section');
+const Contact = document.querySelector('#contacts');
+
+function ListDisplay() {
+  BookSection.style.display = 'block';
+  MainTitle.style.display = 'block';
+  AddSection.style.display = 'none';
+  Contact.style.display = 'none';
+}
+
+export function NewBook() {
+  console.log('it has displayed');
+  AddSection.style.display = 'block';
+  Contact.style.display = 'none';
+  BookSection.style.display = 'none';
+  MainTitle.style.display = 'none';
+}
+
+function Getinfo() {
+  Contact.style.display = 'block';
+  AddSection.style.display = 'none';
+  BookSection.style.display = 'none';
+  MainTitle.style.display = 'none';
+}
+
+ShowList.addEventListener('click', ListDisplay);
+AddNew.addEventListener('click', NewBook);
+ContactInfo.addEventListener('click', Getinfo);
