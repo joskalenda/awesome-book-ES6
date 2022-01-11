358552f84d69c6c254936b3691c5691e89c09615
Topaz Jos <topazjos@Topaz.local>
2022-01-11 14:01:13 +0200

    Arrow function implementation

--- src/navbar.js
+++ src/navbar.js
@@ -3,21 +3,21 @@ const MainTitle = document.querySelector('#list-title');
 const AddSection = document.querySelector('.add-section');
 const Contact = document.querySelector('#contacts');
 
-export function ListDisplay() {
+export const ListDisplay = () => {
   BookSection.style.display = 'block';
   MainTitle.style.display = 'block';
   AddSection.style.display = 'none';
   Contact.style.display = 'none';
 }
 
-export function NewBook() {
+export const NewBook = () => {
   AddSection.style.display = 'block';
   Contact.style.display = 'none';
   BookSection.style.display = 'none';
   MainTitle.style.display = 'none';
 }
 
-export function Getinfo() {
+export const Getinfo = () => {
   Contact.style.display = 'block';
   AddSection.style.display = 'none';
   BookSection.style.display = 'none';
